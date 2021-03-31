ca6dcc63a1960604f9205d8e76ea09934fb23709
Abdulrahman <aalsoheim@gmail.com>
2021-04-01 01:06:40 +0300

    handle errors and documentation

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -33,6 +33,9 @@ def create_app(test_config=None):
   '''
   @app.route('/categories', methods=['GET'])
   def get_categories():
+    """
+    This function handles requesting all availabe categories.
+    """
     # get all categories
     categories = Category.query.all()
     # abort if no categories available
@@ -62,7 +65,7 @@ def create_app(test_config=None):
   @app.route('/questions', methods=['GET'])
   def get_questions():
     """
-    This function handles GET requests for /questions endpoint.
+    This function handles requests for paginated questions.
     """
     # get all questions
     questions = Question.query.all()
@@ -97,6 +100,9 @@ def create_app(test_config=None):
   '''
   @app.route('/questions/<int:question_id>', methods=['DELETE'])
   def delete_question(question_id):
+    """
+    This function handles deleting requested question.
+    """
     question = Question.query.get_or_404(question_id)
     question.delete()
 
@@ -116,16 +122,37 @@ def create_app(test_config=None):
   '''
   @app.route('/questions', methods=['POST'])
   def add_question():
-    if request.get_json() is not None:
-      question = Question(request.get_json()['question'],
-                          request.get_json()['answer'],
-                          request.get_json()['category'],
-                          request.get_json()['difficulty'])
+    """
+    This function handles inserting a new question.
+    """
+    difficulties = [1, 2, 3, 4, 5]
+    categories = get_categories().json
+    
+    data = request.get_json()
+    # check if request data is valid
+    if data is None:
+      abort(400, 'Data is empty!')
+    elif ('question' or 
+          'answer' or 
+          'category' or 
+          'difficulty') not in data.keys():
+      abort(400, 'Invalid data format!')
+    elif len(data['question']) < 1:
+      abort(400, 'Question is empty!')
+    elif len(data['answer']) < 1:
+      abort(400, 'Answer is empty!')
+    elif data['category'] not in categories.values():
+      abort(400, 'Invalid category value!')
+    elif data['difficulty'] not in difficulties:
+      abort(400, 'Invalid difficulty value!')  
+    else:
+      question = Question(data['question'],
+                          data['answer'],
+                          data['category'],
+                          data['difficulty'])
       
       question.insert()
-    else:
-      abort(400)
-
+      
     return jsonify({
       'success': True
     })
@@ -141,9 +168,13 @@ def create_app(test_config=None):
   '''
   @app.route('/questions/search', methods=['POST'])
   def search_questions():
+    """
+    This function handles requested search for questions.
+    """
+    # get search term and category if selected
     search_term = request.get_json()['searchTerm']
     current_category = request.args.get('category', None, type=int)
-    
+    # check if a category is selected or not
     if current_category is None:
       questions = Question.query\
                   .filter(Question.question.ilike(f'%{search_term}%')).all()
@@ -151,7 +182,7 @@ def create_app(test_config=None):
       questions = Question.query\
                   .filter(Question.question.ilike(f'%{search_term}%'))\
                   .filter(Question.category == current_category).all()
-
+    # format questions
     formatted_questions = [question.format() for question in questions]
     return jsonify({
       'success': True,
@@ -168,8 +199,14 @@ def create_app(test_config=None):
   '''
   @app.route('/categories/<int:cat_id>/questions', methods=['GET'])
   def get_category_questions(cat_id):
+    """
+    This function handles requests for questions based on on a category.
+    """
+    # check category availability
     current_category = Category.query.get_or_404(cat_id)
-    questions = Question.query.filter(Question.category == current_category.id).all()
+    questions = Question.query\
+                .filter(Question.category == str(cat_id))\
+                .all()
     formatted_questions = [question.format() for question in questions]
     return jsonify({
       'success': True,
@@ -192,17 +229,25 @@ def create_app(test_config=None):
   '''
   @app.route('/quizzes', methods=['POST'])
   def select_random_question():
+    """
+    This function handles the process of selecting a random question
+    based on a category if specified, and previous questions
+    to eleminate repetition.
+    """
     data = request.get_json()
-    print(data, type(data))
+    # check if request data is valid 
     if data is not None:
+      # check dictionary is in correct format
       if ('previous_questions' or 'quiz_category') not in data.keys():
         abort(400, f'{data},\'previous_questions\' and/or \'quiz_category\' are missing!')
 
       previous_questions = data['previous_questions']
       quiz_category = data['quiz_category']
-      
+      # check all values are valid
       if quiz_category is None or not isinstance(quiz_category, dict):
         abort(400, f'{data},\'quiz_category\' is None or not a dictionary!')
+      elif not isinstance(previous_questions, list):
+        abort(400, f'{previous_questions}, \'previous_questions\' is not a list!')
       elif 'id' not in quiz_category.keys():
         abort(400, f'{quiz_category}, \'id\' key is missing!')
       elif type(quiz_category['id']) is not int:
@@ -213,7 +258,7 @@ def create_app(test_config=None):
                   .order_by(func.random()).first()
       else:
         question = Question.query\
-                  .filter(Question.category == quiz_category['id'])\
+                  .filter(Question.category == str(quiz_category['id']))\
                   .filter(Question.id.notin_(previous_questions))\
                   .order_by(func.random()).first()
       
